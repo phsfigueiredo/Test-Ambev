ff36f4f44a8a031bd66911594a1a44090a32e6cb
Pedro Figueiredo <Pedro.Figueiredo-ext@ab-Inbev.com>
2024-12-06 16:30:10 -0300

    page e actions

--- cypress/e2e/backend_tests/deletar-usuarios_tests.cy.js
+++ cypress/e2e/backend_tests/deletar-usuarios_tests.cy.js
@@ -3,32 +3,29 @@ describe('Deletar e listar usuários', () => {
     it('Deletar falha', () => {
         cy.cadastroUsuariofalha().then((response) => {
             console.log(response);
+            expect(response.status).to.not.equal(201);
+           
         });
     });
 
     it('Deletar sucesso', () => {
-        cy.request({
-            url: '/usuarios',
-            method: 'GET'
-        }).then((response) => {
-            let id = response.body.usuarios[1]._id;
+        cy.getUsuarios().then((usuarios) => {
+          const id = usuarios[1]._id;
+          cy.deletarUsuario(id).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.message).to.eql("Registro excluído com sucesso");
+    
             cy.request({
-                url: 'usuarios/' + id,
-                method: 'DELETE'
+              url: `/usuarios/${id}`,
+              method: 'GET',
+              failOnStatusCode: false // Adiciona esta linha para não falhar em status 400
             }).then((response) => {
-                expect(response.status).to.eq(200);
-                expect(response.body.message).to.eql("Registro excluído com sucesso");
-                
-                cy.request({
-                    url: '/usuarios/' + id,
-                    method: 'GET',
-                    failOnStatusCode: false // Adiciona esta linha para não falhar em status 400
-                }).then((response) => {
-                    // No response não deve haver nada, pois o ID foi excluído
-                    expect(response.status).to.eq(400);
-                    expect(response.body.message).to.eql("Usuário não encontrado");
-                });
+              // No response não deve haver nada, pois o ID foi excluído
+              expect(response.status).to.eq(400);
+              expect(response.body.message).to.eql("Usuário não encontrado");
             });
+          });
         });
+      });
     });
-});
+    
--- cypress/e2e/backend_tests/login_tests.cy.js
+++ cypress/e2e/backend_tests/login_tests.cy.js
@@ -1,17 +1,26 @@
-describe('Teste de Login APPI', () => {
+// cypress/e2e/login_tests.cy.js
+
+describe('Teste de Login API', () => {
 
     it('Login POST sucesso', () => {
-    cy.login("fulano@qa.com", "teste").then((response) => {
-        console.log(response)
-    })
+      cy.login("fulano@qa.com", "teste").then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.statusText).to.eq("OK");
+        expect(response.body.message).to.eq("Login realizado com sucesso");
+        cy.log(response.body.authorization);
+        console.log(response);
+      });
     });
-
-
+  
     it('Login POST falha', () => {
-        cy.loginfailed("pedro@qa.com", "teste").then((response) => {
-            console.log(response)
-        })
-        });
-    
-
-});
+      cy.loginfailed("pedro@qa.com", "teste").then((response) => {
+        expect(response.status).to.not.eq(200);
+        expect(response.statusText).to.not.eq("OK");
+        expect(response.body.message).to.not.eq("Login realizado com sucesso");
+        cy.log('Login falhou conforme esperado');
+        console.log(response);
+      });
+    });
+  
+  });
+  
